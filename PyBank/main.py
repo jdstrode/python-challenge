2f8c48c090e5d6e92fed25490a12197ef2cab2bc
John Strode <jdstrode@gmail.com>
2021-03-09 12:18:15 -0600

    updated variables

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,8 +16,8 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 totalmonths = 0
 monthofchange= []
 netchange= []
-greatestincrease = ["",0] #credit to classmate, Daniel Atuesta, to avoid indexerror
-greatestdecrease = ["", 9999999999999] #credit to classmate, Daniel Atuesta, to avoid indexerror
+grtinc = ["",0] #credit to classmate, Daniel Atuesta, to avoid indexerror
+grtdec = ["", 9999999999999] #credit to classmate, Daniel Atuesta, to avoid indexerror
 netprofits = 0
 
 # Open the file using "write" mode. Specify the variable to hold the contents
@@ -28,7 +28,7 @@ with open(csvpath) as csvfile:
     first_row = next(csvreader)
     totalmonths = totalmonths + 1
     netprofits = netprofits + int(first_row[1])
-    previousnet = int(first_row[1])
+    prevnet = int(first_row[1])
     
     # loop through rows in csvreader
     for row in csvreader:
@@ -41,23 +41,23 @@ with open(csvpath) as csvfile:
 
         # save the values of all the differences from value in second column to the next value in that column into a list [netchange]
         # classmate Daniel Atuesta assisted with some of this framework
-        netchangevalue = int(row[1]) - previousnet
-        previousnet = int(row[1])
+        netchangevalue = int(row[1]) - prevnet
+        prevnet = int(row[1])
         netchange += [netchangevalue]
         # save the months from each difference into a similar list [monthofchange]
         monthofchange = monthofchange + [row[0]]
 
         # create if statement to compare netchangevalue (which is aggregated everytime we loop through table above) and save the final greatest value into greatestincrease variable
-        if netchangevalue > greatestincrease[1]:
-            greatestincrease[0] = row[0]
-            greatestincrease[1] = netchangevalue
+        if netchangevalue > grtinc[1]:
+            grtinc[0] = row[0]
+            grtinc[1] = netchangevalue
         # complete the same process as in last step, but save into greatestdecrease variable
-        if netchangevalue < greatestdecrease[1]:
-            greatestdecrease[0] = row[0]
-            greatestdecrease [1] = netchangevalue
+        if netchangevalue < grtdec[1]:
+            grtdec[0] = row[0]
+            grtdec[1] = netchangevalue
 
     # use the aggregated list of netchange, sum the totals within it, and divide by its length to get the average
-    averagechange = sum(netchange)/ len(netchange)
+    avgchange = sum(netchange)/ len(netchange)
 
 # Specify the file to WRITE to
 output_path = os.path.join("analysis", "results1.txt")
@@ -87,15 +87,15 @@ with open(output_path, 'w', newline='', encoding='utf8') as txtfile:
     print(f"Total: {str(netprofits)}")
 
     # Write average change in profit/losses
-    writer.writerow([f"Average Change: ${str(averagechange)}"])
+    writer.writerow([f"Average Change: ${str(avgchange)}"])
     # print to terminal
-    print(f"Average Change: ${str(averagechange)}")
+    print(f"Average Change: ${str(avgchange)}")
 
     # Write greatest increase in profits (date and amount) over entire period
-    writer.writerow([f"Greatest Increase in Profits: {greatestincrease[0]} (${str(greatestincrease[1])})"])
-    print(f"Greatest Increase in Profits: {greatestincrease[0]} (${str(greatestincrease[1])})")
+    writer.writerow([f"Greatest Increase in Profits: {grtinc[0]} (${str(grtinc[1])})"])
+    print(f"Greatest Increase in Profits: {grtinc[0]} (${str(grtinc[1])})")
 
     # Write greatest decrease in losses (date and amount) over entire period
-    writer.writerow([f"Greatest Increase in Profits: {greatestdecrease[0]} (${str(greatestdecrease[1])})"])
+    writer.writerow([f"Greatest Increase in Profits: {grtdec[0]} (${str(grtdec[1])})"])
     # print to terminal
-    print(f"Greatest Increase in Profits: {greatestdecrease[0]} (${str(greatestdecrease[1])})")
+    print(f"Greatest Increase in Profits: {grtdec[0]} (${str(grtdec[1])})")
